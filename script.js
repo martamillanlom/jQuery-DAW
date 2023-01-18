10f2bdbb9e36dc5804022e9941e1a6baf5dfd458
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2023-01-18 16:07:04 +0100

    jQueryDAW

---
+++ script.js
@@ -0,0 +1,14 @@
+let projectes;
+let i=0;
+
+
+//1. Crea una funció que llegeixi tots els projectes de l'arxiu data/projectes.json
+
+
+//2. Fes una funció que mostri la informació a la pantalla
+
+
+//Funció per calcular l'amplada en el cas que la posició sigui center
+function calcWidth(width){
+    
+}
